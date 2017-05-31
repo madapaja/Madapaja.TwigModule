30b54a6279c98d2746836dc2e719eff5e5a3c6f7
IWASAKI Koji <iwasaki@cresc.com>
2017-05-31 18:15:09 +0900

    add tests

---
+++ tests/OptionProviderTestModule.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Madapaja\TwigModule;
+
+use BEAR\AppMeta\AbstractAppMeta;
+use BEAR\AppMeta\AppMeta;
+use Madapaja\TwigModule\Annotation\TwigDebug;
+use Madapaja\TwigModule\Annotation\TwigOptions;
+use Ray\Di\AbstractModule;
+
+class OptionProviderTestModule extends AbstractModule
+{
+    private $tmpDir;
+    private $isDebug;
+
+    /**
+     * @param string $tmpDir
+     * @param bool $isDebug
+     */
+    public function __construct($tmpDir, $isDebug)
+    {
+        $this->tmpDir = $tmpDir;
+        $this->isDebug = $isDebug;
+        parent::__construct(null);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $appMeta = new AppMeta('Madapaja\TwigModule');
+        $appMeta->tmpDir = $this->tmpDir;
+
+        $this->bind(AbstractAppMeta::class)->toInstance($appMeta);
+        $this->install(new TwigModule());
+        $this->bind()->annotatedWith(TwigOptions::class)->toProvider(OptionProvider::class);
+        $this->bind()->annotatedWith(TwigDebug::class)->toInstance($this->isDebug);
+    }
+}
