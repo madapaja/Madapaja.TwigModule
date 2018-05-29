1f50bd98b958272ec5e5033376a73cd9098eb42e
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-05-30 07:23:07 +0900

    bind TwigErrorPath

--- src/TwigErrorPageModule.php
+++ src/TwigErrorPageModule.php
@@ -8,6 +8,7 @@ namespace Madapaja\TwigModule;
 
 use BEAR\Resource\RenderInterface;
 use BEAR\Sunday\Extension\Error\ErrorInterface;
+use Madapaja\TwigModule\Annotation\TwigErrorPath;
 use Ray\Di\AbstractModule;
 
 class TwigErrorPageModule extends AbstractModule
@@ -19,6 +20,7 @@ class TwigErrorPageModule extends AbstractModule
     {
         $this->bind(RenderInterface::class)->annotatedWith('error_page')->to(ErrorPagerRenderer::class);
         $this->bind(ErrorInterface::class)->to(TwigErrorHandler::class);
+        $this->bind()->annotatedWith(TwigErrorPath::class)->toInstance('/error/error.html.twig');
         $this->bind(TwigErrorPage::class);
     }
 }
