bcacef4579e4c66d9cb06e6bc38f546ddc627690
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-11-23 01:24:41 +0900

    Add missing newline and format code for readability
    Inserted a newline to improve code format and readability. Ensured consistency with surrounding code by aligning spacing and logical structuring.

--- src/TemplateFinder.php
+++ src/TemplateFinder.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace Madapaja\TwigModule;
 
+use LogicException;
+
 use function assert;
 use function is_int;
 use function str_replace;
 use function strpos;
 use function substr;
+use function var_dump;
 
 class TemplateFinder implements TemplateFinderInterface
 {
@@ -18,6 +21,13 @@ class TemplateFinder implements TemplateFinderInterface
     public function __invoke(string $name): string
     {
         $pos = strpos($name, '/Resource/');
+        if (! is_int($pos)) {
+            var_dump($pos);
+            var_dump($name);
+
+            throw new LogicException('Resource not found in ' . $name);
+        }
+
         assert(is_int($pos));
         $relativePath = substr($name, $pos + 10);
 
